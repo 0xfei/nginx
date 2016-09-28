4f2f1ff4c8dfc9983f4bfe95e5760ece8b2d1a92
0xfei <hhxlovety@gmail.com>
2016-09-28 17:25:23 +0800

    ngx_hash.c ngx_hash.h

--- src/core/ngx_hash.h
+++ src/core/ngx_hash.h
@@ -12,26 +12,27 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 
-
+// hash item
 typedef struct {
     void             *value;
     u_short           len;
     u_char            name[1];
 } ngx_hash_elt_t;
 
-
+// hash struct
 typedef struct {
-    ngx_hash_elt_t  **buckets;
-    ngx_uint_t        size;
+    ngx_hash_elt_t  **buckets;  // buckets[size] -> *ngx_hash_elt_t
+    ngx_uint_t        size;     // max item
 } ngx_hash_t;
 
-
+// wildcard hash struct
 typedef struct {
     ngx_hash_t        hash;
     void             *value;
 } ngx_hash_wildcard_t;
 
 
+// key-value pair
 typedef struct {
     ngx_str_t         key;
     ngx_uint_t        key_hash;
@@ -39,9 +40,11 @@ typedef struct {
 } ngx_hash_key_t;
 
 
+// hash counter
 typedef ngx_uint_t (*ngx_hash_key_pt) (u_char *data, size_t len);
 
 
+// three hash structs
 typedef struct {
     ngx_hash_t            hash;
     ngx_hash_wildcard_t  *wc_head;
@@ -49,15 +52,16 @@ typedef struct {
 } ngx_hash_combined_t;
 
 
+// hash initialize struct
 typedef struct {
-    ngx_hash_t       *hash;
-    ngx_hash_key_pt   key;
+    ngx_hash_t       *hash;         // ngx_hash_t
+    ngx_hash_key_pt   key;          // key calculator
 
-    ngx_uint_t        max_size;
-    ngx_uint_t        bucket_size;
+    ngx_uint_t        max_size;     // max items
+    ngx_uint_t        bucket_size;  // bucket_size, the size of values with same key
 
-    char             *name;
-    ngx_pool_t       *pool;
+    char             *name;         // name of this struct
+    ngx_pool_t       *pool;         // pool
     ngx_pool_t       *temp_pool;
 } ngx_hash_init_t;
 
@@ -108,12 +112,14 @@ ngx_int_t ngx_hash_init(ngx_hash_init_t *hinit, ngx_hash_key_t *names,
 ngx_int_t ngx_hash_wildcard_init(ngx_hash_init_t *hinit, ngx_hash_key_t *names,
     ngx_uint_t nelts);
 
+// calculate hash
 #define ngx_hash(key, c)   ((ngx_uint_t) key * 31 + c)
 ngx_uint_t ngx_hash_key(u_char *data, size_t len);
 ngx_uint_t ngx_hash_key_lc(u_char *data, size_t len);
 ngx_uint_t ngx_hash_strlow(u_char *dst, u_char *src, size_t n);
 
 
+// real used
 ngx_int_t ngx_hash_keys_array_init(ngx_hash_keys_arrays_t *ha, ngx_uint_t type);
 ngx_int_t ngx_hash_add_key(ngx_hash_keys_arrays_t *ha, ngx_str_t *key,
     void *value, ngx_uint_t flags);
